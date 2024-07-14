32067ee9e0dc7fd83ed783937f0bc178f12b356a
DanitSi112 <danit.sigal@gmail.com>
2024-07-14 12:40:54 +0300

    added flywheel set power command

---
+++ src/main/java/frc/robot/commands/FlywheelCommands.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import frc.robot.subsystems.flywheel.Flywheel;
+
+public class FlywheelCommands {
+    private Flywheel flywheel;
+
+    public FlywheelCommands(Flywheel flywheel){
+        this.flywheel=flywheel;
+    } 
+
+    public Command getSetPowerCommand(double power){
+        return new FunctionalCommand(
+            () -> {},
+            () -> flywheel.setPower(power),
+            (interrupted) -> flywheel.setPower(0),
+            () -> false,
+            flywheel
+        );
+
+    }
+}
